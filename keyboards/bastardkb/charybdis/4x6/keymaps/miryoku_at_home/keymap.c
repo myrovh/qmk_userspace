43dc17e88e0b378b6d248a43800bf53b105c22b9
myrovh <admin@myrovh.dev>
2024-11-02 16:20:48 +1100

    all layers setup

--- keyboards/bastardkb/charybdis/4x6/keymaps/miryoku_at_home/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/miryoku_at_home/keymap.c
@@ -22,9 +22,11 @@
 
 enum charybdis_keymap_layers {
     LAYER_BASE = 0,
-    LAYER_LOWER,
-    LAYER_RAISE,
-    LY_MOUSE,
+    LY_NAV,
+    LY_NUM,
+    LY_SYM,
+    LY_FUN,
+    LY_MOUSE
 };
 
 /** \brief Automatically enable sniping-mode on the pointer layer. */
@@ -42,11 +44,19 @@ static uint16_t auto_pointer_layer_timer = 0;
 #    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD
 #endif     // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 
-#define LOWER MO(LAYER_LOWER)
-#define RAISE MO(LAYER_RAISE)
 #define PT_Z LT(LY_MOUSE, KC_Z)
 #define PT_SLSH LT(LY_MOUSE, KC_SLSH)
+#define NAV LT(LY_NAV, KC_SPC)
+#define NUM LT(LY_NUM, KC_BSPC)
+#define SYM LT(LY_SYM, KC_ENT)
+#define FUN LT(LY_FUN, KC_DEL)
 #define XXX KC_NO
+
+#define U_RDO C(KC_Y)
+#define U_PST C(KC_V)
+#define U_CPY C(KC_C)
+#define U_CUT C(KC_X)
+#define U_UND C(KC_Z)
 
 #ifndef POINTING_DEVICE_ENABLE
 #    define DRGSCRL KC_NO
@@ -62,38 +72,44 @@ XXX, XXX         , XXX         , XXX         , XXX         , XXX   , XXX    , XX
 XXX, KC_Q        , KC_W        , KC_E        , KC_R        , KC_T  , KC_Y   , KC_U        , KC_I        , KC_O          , KC_P           , XXX
 XXX, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G  , KC_H   , LSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L)  , LGUI_T(KC_QUOT), XXX
 XXX, PT_Z        , ALGR_T(KC_X), KC_C        , KC_V        , KC_B  , KC_N   , KC_M        , KC_COMM     , ALGR_T(KC_DOT), PT_SLSH        , XXX
-                                 XXX         , KC_ESC      , KC_SPC, KC_BSPC, KC_DEL,
-                                               XXX         , KC_TAB, KC_ENT
+                                 XXX         , KC_ESC      , NAV   , NUM    , FUN         ,
+                                               XXX         , KC_TAB, SYM
   ),
 
-  [LAYER_LOWER] = LAYOUT(
-  // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-       KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
-  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       RGB_MOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_LBRC,   KC_P7,   KC_P8,   KC_P9, KC_RBRC, XXXXXXX,
-  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       RGB_TOG, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    KC_PPLS,   KC_P4,   KC_P5,   KC_P6, KC_PMNS, KC_PEQL,
-  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-      RGB_RMOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_PAST,   KC_P1,   KC_P2,   KC_P3, KC_PSLS, KC_PDOT,
-  // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  XXXXXXX, XXXXXXX, _______,    XXXXXXX, _______,
-                                           XXXXXXX, XXXXXXX,      KC_P0
-  //                            ╰───────────────────────────╯ ╰──────────────────╯
+  [LY_NAV] = LAYOUT(
+XXX, XXX         , XXX         , XXX         , XXX         , XXX   , XXX    , XXX         , XXX         , XXX           , XXX            , XXX
+XXX, XXX         , XXX         , XXX         , XXX         , XXX   , U_RDO  , U_PST       , U_CPY       , U_CUT         , U_UND          , XXX
+XXX, KC_LGUI     , KC_LALT     , KC_LCTL     , KC_LSFT     , XXX   , KC_LEFT, KC_DOWN     , KC_UP       , KC_RGHT       , CW_TOGG        , XXX
+XXX, XXX         , KC_ALGR     , XXX         , XXX         , XXX   , KC_HOME, KC_PGDN     , KC_PGUP     , KC_END        , KC_INS         , XXX
+                                 XXX         , XXX         , XXX   , KC_BSPC, KC_DEL      ,
+                                               XXX         , XXX   , KC_ENT
   ),
 
-  [LAYER_RAISE] = LAYOUT(
-  // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-        KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_MNXT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLU,
-  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_MPLY, KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, KC_MUTE,
-  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_MPRV, KC_HOME, KC_PGUP, KC_PGDN,  KC_END, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD,
-  // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  _______, _______, XXXXXXX,    _______, XXXXXXX,
-                                           _______, _______,    XXXXXXX
-  //                            ╰───────────────────────────╯ ╰──────────────────╯
+  [LY_NUM] = LAYOUT(
+XXX, XXX         , XXX         , XXX         , XXX         , XXX   , XXX    , XXX         , XXX         , XXX           , XXX            , XXX
+XXX, KC_LBRC     , KC_7        , KC_8        , KC_9        ,KC_RBRC, XXX    , XXX         , XXX         , XXX           , XXX            , XXX
+XXX, KC_SCLN     , KC_4        , KC_5        , KC_6        , KC_EQL, XXX    , KC_LSFT     , KC_LCTL     , KC_LALT       , KC_LGUI        , XXX
+XXX, KC_GRV      , KC_1        , KC_2        , KC_3        ,KC_BSLS, XXX    , XXX         , XXX         , KC_ALGR       , XXX            , XXX
+                                 XXX         , KC_DOT      , KC_0  , XXX    , XXX         ,
+                                               XXX         ,KC_MINS, XXX
+  ),
+
+  [LY_SYM] = LAYOUT(
+XXX, XXX         , XXX         , XXX         , XXX         , XXX   , XXX    , XXX         , XXX         , XXX           , XXX            , XXX
+XXX, KC_LCBR     , KC_AMPR     , KC_ASTR     , KC_LPRN     ,KC_RCBR, XXX    , XXX         , XXX         , XXX           , XXX            , XXX
+XXX, KC_COLN     , KC_DLR      , KC_PERC     , KC_CIRC     ,KC_PLUS, XXX    , KC_LSFT     , KC_LCTL     , KC_LALT       , KC_LGUI        , XXX
+XXX, KC_TILD     , KC_EXLM     , KC_AT       , KC_HASH     ,KC_PIPE, XXX    , XXX         , XXX         , KC_ALGR       , XXX            , XXX
+                                 XXX         , KC_LPRN     ,KC_RPRN, XXX    , XXX         ,
+                                               XXX         ,KC_UNDS, XXX
+  ),
+
+  [LY_FUN] = LAYOUT(
+XXX, XXX         , XXX         , XXX         , XXX         , XXX   , XXX    , XXX         , XXX         , XXX           , XXX            , XXX
+XXX, KC_F12      , KC_F7       , KC_F8       , KC_F9       ,KC_PSCR, XXX    , XXX         , XXX         , XXX           , XXX            , XXX
+XXX, KC_F11      , KC_F4       , KC_F5       , KC_F6       ,KC_SCRL, XXX    , KC_LSFT     , KC_LCTL     , KC_LALT       , KC_LGUI        , XXX
+XXX, KC_F10      , KC_F1       , KC_F2       , KC_F3       ,KC_PAUS, XXX    , XXX         , XXX         , KC_ALGR       , XXX            , XXX
+                                 XXX         , KC_APP      , KC_SPC, XXX    , XXX         ,
+                                               XXX         ,KC_TAB , XXX
   ),
 
   [LY_MOUSE] = LAYOUT(
