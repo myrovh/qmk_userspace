ace4e8b5b2182488e9692034602054ebcc802529
myrovh <admin@myrovh.dev>
2024-11-02 22:45:23 +1100

    fix layout arrays

--- keyboards/bastardkb/charybdis/4x6/keymaps/miryoku_at_home/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/miryoku_at_home/keymap.c
@@ -14,6 +14,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <complex.h>
+#include "charybdis.h"
+#include "config.h"
 #include QMK_KEYBOARD_H
 
 #ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
@@ -68,46 +71,46 @@ static uint16_t auto_pointer_layer_timer = 0;
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT(
-XXX, XXX         , XXX         , XXX         , XXX         , XXX   , XXX    , XXX         , XXX         , XXX           , XXX            , XXX
-XXX, KC_Q        , KC_W        , KC_E        , KC_R        , KC_T  , KC_Y   , KC_U        , KC_I        , KC_O          , KC_P           , XXX
-XXX, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G  , KC_H   , LSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L)  , LGUI_T(KC_QUOT), XXX
-XXX, PT_Z        , ALGR_T(KC_X), KC_C        , KC_V        , KC_B  , KC_N   , KC_M        , KC_COMM     , ALGR_T(KC_DOT), PT_SLSH        , XXX
+XXX, XXX         , XXX         , XXX         , XXX         , XXX   , XXX    , XXX         , XXX         , XXX           , XXX            , XXX ,
+XXX, KC_Q        , KC_W        , KC_E        , KC_R        , KC_T  , KC_Y   , KC_U        , KC_I        , KC_O          , KC_P           , XXX ,
+XXX, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G  , KC_H   , LSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L)  , LGUI_T(KC_QUOT), XXX ,
+XXX, PT_Z        , ALGR_T(KC_X), KC_C        , KC_V        , KC_B  , KC_N   , KC_M        , KC_COMM     , ALGR_T(KC_DOT), PT_SLSH        , XXX ,
                                  XXX         , KC_ESC      , NAV   , NUM    , FUN         ,
                                                XXX         , KC_TAB, SYM
   ),
 
   [LY_NAV] = LAYOUT(
-XXX, XXX         , XXX         , XXX         , XXX         , XXX   , XXX    , XXX         , XXX         , XXX           , XXX            , XXX
-XXX, XXX         , XXX         , XXX         , XXX         , XXX   , U_RDO  , U_PST       , U_CPY       , U_CUT         , U_UND          , XXX
-XXX, KC_LGUI     , KC_LALT     , KC_LCTL     , KC_LSFT     , XXX   , KC_LEFT, KC_DOWN     , KC_UP       , KC_RGHT       , CW_TOGG        , XXX
-XXX, XXX         , KC_ALGR     , XXX         , XXX         , XXX   , KC_HOME, KC_PGDN     , KC_PGUP     , KC_END        , KC_INS         , XXX
+XXX, XXX         , XXX         , XXX         , XXX         , XXX   , XXX    , XXX         , XXX         , XXX           , XXX            , XXX ,
+XXX, XXX         , XXX         , XXX         , XXX         , XXX   , U_RDO  , U_PST       , U_CPY       , U_CUT         , U_UND          , XXX ,
+XXX, KC_LGUI     , KC_LALT     , KC_LCTL     , KC_LSFT     , XXX   , KC_LEFT, KC_DOWN     , KC_UP       , KC_RGHT       , CW_TOGG        , XXX ,
+XXX, XXX         , KC_ALGR     , XXX         , XXX         , XXX   , KC_HOME, KC_PGDN     , KC_PGUP     , KC_END        , KC_INS         , XXX ,
                                  XXX         , XXX         , XXX   , KC_BSPC, KC_DEL      ,
                                                XXX         , XXX   , KC_ENT
   ),
 
   [LY_NUM] = LAYOUT(
-XXX, XXX         , XXX         , XXX         , XXX         , XXX   , XXX    , XXX         , XXX         , XXX           , XXX            , XXX
-XXX, KC_LBRC     , KC_7        , KC_8        , KC_9        ,KC_RBRC, XXX    , XXX         , XXX         , XXX           , XXX            , XXX
-XXX, KC_SCLN     , KC_4        , KC_5        , KC_6        , KC_EQL, XXX    , KC_LSFT     , KC_LCTL     , KC_LALT       , KC_LGUI        , XXX
-XXX, KC_GRV      , KC_1        , KC_2        , KC_3        ,KC_BSLS, XXX    , XXX         , XXX         , KC_ALGR       , XXX            , XXX
+XXX, XXX         , XXX         , XXX         , XXX         , XXX   , XXX    , XXX         , XXX         , XXX           , XXX            , XXX ,
+XXX, KC_LBRC     , KC_7        , KC_8        , KC_9        ,KC_RBRC, XXX    , XXX         , XXX         , XXX           , XXX            , XXX ,
+XXX, KC_SCLN     , KC_4        , KC_5        , KC_6        , KC_EQL, XXX    , KC_LSFT     , KC_LCTL     , KC_LALT       , KC_LGUI        , XXX ,
+XXX, KC_GRV      , KC_1        , KC_2        , KC_3        ,KC_BSLS, XXX    , XXX         , XXX         , KC_ALGR       , XXX            , XXX ,
                                  XXX         , KC_DOT      , KC_0  , XXX    , XXX         ,
                                                XXX         ,KC_MINS, XXX
   ),
 
   [LY_SYM] = LAYOUT(
-XXX, XXX         , XXX         , XXX         , XXX         , XXX   , XXX    , XXX         , XXX         , XXX           , XXX            , XXX
-XXX, KC_LCBR     , KC_AMPR     , KC_ASTR     , KC_LPRN     ,KC_RCBR, XXX    , XXX         , XXX         , XXX           , XXX            , XXX
-XXX, KC_COLN     , KC_DLR      , KC_PERC     , KC_CIRC     ,KC_PLUS, XXX    , KC_LSFT     , KC_LCTL     , KC_LALT       , KC_LGUI        , XXX
-XXX, KC_TILD     , KC_EXLM     , KC_AT       , KC_HASH     ,KC_PIPE, XXX    , XXX         , XXX         , KC_ALGR       , XXX            , XXX
+XXX, XXX         , XXX         , XXX         , XXX         , XXX   , XXX    , XXX         , XXX         , XXX           , XXX            , XXX ,
+XXX, KC_LCBR     , KC_AMPR     , KC_ASTR     , KC_LPRN     ,KC_RCBR, XXX    , XXX         , XXX         , XXX           , XXX            , XXX ,
+XXX, KC_COLN     , KC_DLR      , KC_PERC     , KC_CIRC     ,KC_PLUS, XXX    , KC_LSFT     , KC_LCTL     , KC_LALT       , KC_LGUI        , XXX ,
+XXX, KC_TILD     , KC_EXLM     , KC_AT       , KC_HASH     ,KC_PIPE, XXX    , XXX         , XXX         , KC_ALGR       , XXX            , XXX ,
                                  XXX         , KC_LPRN     ,KC_RPRN, XXX    , XXX         ,
                                                XXX         ,KC_UNDS, XXX
   ),
 
   [LY_FUN] = LAYOUT(
-XXX, XXX         , XXX         , XXX         , XXX         , XXX   , XXX    , XXX         , XXX         , XXX           , XXX            , XXX
-XXX, KC_F12      , KC_F7       , KC_F8       , KC_F9       ,KC_PSCR, XXX    , XXX         , XXX         , XXX           , XXX            , XXX
-XXX, KC_F11      , KC_F4       , KC_F5       , KC_F6       ,KC_SCRL, XXX    , KC_LSFT     , KC_LCTL     , KC_LALT       , KC_LGUI        , XXX
-XXX, KC_F10      , KC_F1       , KC_F2       , KC_F3       ,KC_PAUS, XXX    , XXX         , XXX         , KC_ALGR       , XXX            , XXX
+XXX, XXX         , XXX         , XXX         , XXX         , XXX   , XXX    , XXX         , XXX         , XXX           , XXX            , XXX ,
+XXX, KC_F12      , KC_F7       , KC_F8       , KC_F9       ,KC_PSCR, XXX    , XXX         , XXX         , XXX           , XXX            , XXX ,
+XXX, KC_F11      , KC_F4       , KC_F5       , KC_F6       ,KC_SCRL, XXX    , KC_LSFT     , KC_LCTL     , KC_LALT       , KC_LGUI        , XXX ,
+XXX, KC_F10      , KC_F1       , KC_F2       , KC_F3       ,KC_PAUS, XXX    , XXX         , XXX         , KC_ALGR       , XXX            , XXX ,
                                  XXX         , KC_APP      , KC_SPC, XXX    , XXX         ,
                                                XXX         ,KC_TAB , XXX
   ),
@@ -123,7 +126,7 @@ XXX, KC_F10      , KC_F1       , KC_F2       , KC_F3       ,KC_PAUS, XXX    , XX
        XXXXXXX, XXXXXXX, DRGSCRL, SNIPING, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, SNIPING, DRGSCRL, XXXXXXX, XXXXXXX,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                   KC_BTN2, KC_BTN1, KC_BTN3,    KC_BTN3, KC_BTN1,
-                                           DRGSCRL, KC_BTN2,    KC_BTN2
+                                  DRG_TOG, KC_BTN2,    KC_BTN2
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 };
