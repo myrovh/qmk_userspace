99fbb8409fbcd28e0fabde02c74ca5ae6ed7dae2
myrovh <admin@myrovh.dev>
2025-06-29 23:24:47 +1000

    minor formatting fixup

--- keyboards/bastardkb/charybdis/4x6/keymaps/miryoku_at_home/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/miryoku_at_home/keymap.c
@@ -125,7 +125,7 @@ XXX, KC_F10      , KC_F1       , KC_F2       , KC_F3       ,KC_PAUS, XXX    , XX
        XXXXXXX, XXXXXXX, DRGSCRL, SNIPING, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, SNIPING, DRGSCRL, XXXXXXX, XXXXXXX,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                   KC_BTN2, KC_BTN1, KC_BTN3,    KC_BTN3, KC_BTN1,
-                                  DRG_TOG, KC_BTN2,    KC_BTN2
+                                           DRG_TOG, KC_BTN2,    KC_BTN2
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 };
