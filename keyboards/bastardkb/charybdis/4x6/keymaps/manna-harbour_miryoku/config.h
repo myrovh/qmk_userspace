2054f74ad956ba0d907fd0fe9447f94774694108
myrovh <admin@myrovh.dev>
2025-07-02 23:03:31 +1000

    adjust right thumb positions and enable auto sniping for mouse layer

--- keyboards/bastardkb/charybdis/4x6/keymaps/manna-harbour_miryoku/config.h
+++ keyboards/bastardkb/charybdis/4x6/keymaps/manna-harbour_miryoku/config.h
@@ -18,6 +18,15 @@ XXX, XXX, XXX, XXX, XXX, XXX,                XXX, XXX, XXX, XXX, XXX, XXX, \
 XXX, K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, XXX, \
 XXX, K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, XXX, \
 XXX, K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, XXX, \
-               K32, K33, K34,                K37, K36, \
-                    XXX, K32,                K35 \
+               K32, K33, K34,                K35, K36, \
+                    XXX, K32,                K37 \
 )
+
+/* Charybdis-specific features. */
+
+#ifdef POINTING_DEVICE_ENABLE
+// Automatically enable the pointer layer when moving the trackball.  See also:
+// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS`
+// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD`
+// #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+#endif // POINTING_DEVICE_ENABLE
